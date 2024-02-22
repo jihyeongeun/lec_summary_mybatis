ec302f188693ac556c43985ba251436ca9e5c7a3
jihyeongeun <hyeongeun0924@gmail.com>
2024-02-22 10:37:59 +0900

    2.22 오전수업 mybatis

---
+++ chap03-mapper-element-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/ElementService.java
@@ -0,0 +1,32 @@
+package com.ohgiraffers.section01.xmlmapper;
+
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.List;
+
+import static com.ohgiraffers.common.Template.getSqlSession;
+
+public class ElementService {
+    public void selectResultMapTest() {
+
+        SqlSession sqlSession = getSqlSession();
+        ElementMapper mapper = sqlSession.getMapper(ElementMapper.class);
+
+        List<MenuDTO> menus = mapper.selectResultMapTest();
+        menus.forEach(System.out::println);
+
+        sqlSession.close();
+    }
+
+    public void selectResultMapAssociationTest() {
+        SqlSession sqlSession = getSqlSession();
+        ElementMapper mapper = sqlSession.getMapper(ElementMapper.class);
+
+        List<MenuAndCategoryDTO> menus = mapper.selectResultAssociationTest();
+        menus.forEach(System.out::println);
+        System.out.println("첫번째 메뉴의 필드명 : " + menus.get(0).getCategory().getCategoryName());
+
+        sqlSession.close();
+
+    }
+}
